5a8929e3e0e71619ae22490f83ec10cc3be57855
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-26 19:40:15 -0300

    cap12-addingConnectionSttring

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<StoreContext>(x=>x.UseSqlite());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
